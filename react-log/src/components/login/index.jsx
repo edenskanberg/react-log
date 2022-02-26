16f776c9ba2a73dc8c9f7ccadb34f6848371a39a
Eden Skånberg <eden.89@live.com>
2022-02-26 14:35:15 +0100

    form

---
+++ react-log/src/components/login/index.jsx
@@ -0,0 +1,4 @@
+//det vi importerar från App.jsx kommer imporetera det som finns i login.jsx och reexport it from the App.jsx
+
+export { Login } from "./login";
+export { Register } from "./register";
